e7cfd48fc0ecb378392b02c09b15af0fa64c0054
Larissa Moreira <64422214+Larissamoreiram@users.noreply.github.com>
2020-09-24 09:09:13 -0300

    Create Perguntas.py

---
+++ Perguntas.py
@@ -0,0 +1,150 @@
+print()
+print('QUESTÃO 1')
+print(' É sempre um        recebê-lo em minha casa!')
+print('[a] praser '
+      '[b] prazer '
+      '[c] prasê ')
+print()
+respostas = input('Resposta:')
+if respostas in 'b':
+    print('Acertou!')
+elif respostas in 'a' 'c':
+    print('Errou!')
+else:
+    print('Ops!!! Opção Inválida!')
+
+print()
+print('QUESTÃO 2')
+print(' A          é inimiga da perfeição.')
+print('[a] Pressa '
+      '[b] Presa '
+      '[c] Preça')
+print()
+respostas = input('Resposta:')
+if respostas in 'a':
+    print('Acertou!')
+elif respostas in 'b' 'c':
+    print('Errou!')
+else:
+    print('Ops!!! Opção Inválida!')
+
+print()
+print('QUESTÃO 3')
+print(' Irei tomar um          antes de ir à aula.')
+print('[a] Iorgurte '
+      '[b] Iogurte '
+      '[c] Iorgute ')
+print()
+respostas = input('Resposta:')
+if respostas in 'b':
+    print('Acertou!')
+elif respostas in 'a' 'c':
+    print('Errou!')
+else:
+    print('Ops!!! Opção Inválida!')
+
+print()
+print('QUESTÃO 4')
+print('Hoje fui          no restaurante.')
+print('[a] Almossar '
+      '[b] Almosar'
+      '[c] Almoçar ')
+print()
+respostas = input('Resposta:')
+if respostas in 'c':
+    print('Acertou!')
+elif respostas in 'a' 'b':
+    print('Errou!')
+else:
+    print('Ops!!! Opção Inválida!')
+
+print()
+print('QUESTÃO 5')
+print(' Meu cabelo está horrível, preciso marcar uma hora no...')
+print('[a] Cabeleireiro '
+      '[b] Cabelerero '
+      '[c] Cabelerero ')
+print()
+respostas = input('Resposta:')
+if respostas in 'a':
+    print('Acertou!')
+elif respostas in 'b' 'c':
+    print('Errou!')
+else:
+    print('Ops!!! Opção Inválida!')
+
+print()
+print('QUESTÃO 6')
+print('  Você pode fazer minha...')
+print('[a] sonbrancelha '
+      '[b] sombrancelha '
+      '[c] sobrancelha')
+print()
+respostas = input('Resposta:')
+if respostas in 'c':
+    print('Acertou!')
+elif respostas in 'a' 'b':
+    print('Errou!')
+else:
+    print('Ops!!! Opção Inválida!')
+
+print()
+print('QUESTÃO 7')
+print('Levei 1 kg de alimento não perecível, pois será um evento...')
+print('[a] Beneficente '
+      '[b] Beneificiente '
+      '[c] Beneficiente ')
+print()
+respostas = input('Resposta:')
+if respostas in 'a':
+    print('Acertou!')
+elif respostas in 'b' 'c':
+    print('Errou!')
+else:
+    print('Ops!!! Opção Inválida!')
+
+print()
+print('QUESTÃO 8')
+print(' Você aqui também? Que      ...')
+print('[a] Coecidência' 
+      '[b] Conhecidência '
+      '[c] Coincidência '
+
+print()
+respostas = input('Resposta:')
+if respostas in 'c':
+    print('Acertou!')
+elif respostas in 'a' 'c':
+    print('Errou!')
+else:
+    print('Ops!!! Opção Inválida!')
+
+print()
+print('QUESTÃO 9')
+print(' Meu amigo ganhou na mega-sena e ficou...')
+print('[a] Milhionário '
+      '[b] Milhonário '
+      '[c] Milionário ')
+print()
+respostas = input('Resposta:')
+if respostas in 'c':
+    print('Acertou!')
+elif respostas in 'a' 'b':
+    print('Errou!')
+else:
+    print('Ops!!! Opção Inválida!')
+
+print()
+print('QUESTÃO 10')
+print(' Comprei uma camisa para o meu pai no     .')
+print('[a] Shopping '
+      '[b] Chopping '
+      '[c] Xopping ')
+print()
+respostas = input('Resposta:')
+if respostas in 'a':
+    print('Acertou!')
+elif respostas in 'b' 'c':
+    print('Errou!')
+else:
+    print('Ops!!! Opção Inválida!')
